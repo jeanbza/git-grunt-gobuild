df2fde2f0dd1babcfa35981f0c9bd5548532ad8d
Jean de Klerk <jadekler@gmail.com>
2014-10-19 17:54:45 -0600

    Switched to node child processes for the most part

--- tasks/gocompile.js
+++ tasks/gocompile.js
@@ -1,17 +1,69 @@
 'use strict';
 
+var spawn = require('child_process').spawn;
+
 module.exports = function(grunt) {
+    // Internal lib.
+    var shelljs = require('shelljs');
+    var chalk = require('chalk');
 
-  // Internal lib.
-  var shelljs = require('shelljs');
-  var chalk = require('chalk');
+    var opts = {
+        stdout: function (data) {
+            grunt.log.writeln(data);
+        },
+        stderr: function (data) {
+            grunt.log.error(data);
+        }
+    };
 
-  grunt.registerMultiTask('gocompile', 'Compile Go files.', function() {
-    var binary = shelljs.exec('basename ' + this.data.src).output.replace('.go\n', '');
+    function spawnTasks(done, tasks, opts, finishedCallback) {
+        var task = tasks.shift();
+        var cmd = task.cmd;
+        var args = task.args;
 
-    shelljs.exec('go build '+this.data.src);
-    shelljs.exec('mv '+binary+' '+this.data.dest);
+        console.log("Executing "+cmd+" "+args.join(' '));
 
-    grunt.log.writeln('File ' + chalk.cyan(this.data.src) + ' compiled to ' + chalk.cyan(this.data.dest) + '.');
-  });
+        function spawnFunc() {
+            var proc = spawn(cmd, args, opts);
+
+            proc.on('stdout', function(data) {
+                opts.stdout(data);
+            });
+
+            proc.on('stderr', function(data) {
+                opts.stderr(data);
+            });
+
+            proc.on('exit', function (status) {
+                if (tasks.length === 0) {
+                    finishedCallback();
+                    done();
+                } else {
+                    spawnTasks(done, tasks, opts, finishedCallback)();
+                }
+            });
+        }
+
+        return spawnFunc;
+    }
+
+    grunt.registerMultiTask('gocompile', 'Compile Go files.', function() {
+        var binary = shelljs.exec('basename ' + this.data.src).output.replace('.go\n', '');
+
+        var tasks = [{
+            cmd: 'go',
+            args: ['build', this.data.src]
+        }, {
+            cmd: 'mv',
+            args: [binary, this.data.dest]
+        }];
+
+        var src = this.data.src;
+        var dest = this.data.dest;
+        var finishedCallback = function() {
+            grunt.log.writeln('File ' + chalk.cyan(src) + ' compiled to ' + chalk.cyan(dest) + '.');
+        };
+
+        spawnTasks(this.async(), tasks, opts, finishedCallback)();
+    });
 };
