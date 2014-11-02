dc081f116328d4302133aea0a08e1210d06d1808
Jean de Klerk <jadekler@gmail.com>
2014-11-01 22:02:11 -0600

    Passing tests, but hardcoded GOOS. Why is that being set to windows??

--- tasks/gorun.js
+++ tasks/gorun.js
@@ -1,23 +1,33 @@
 'use strict';
 
 var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 module.exports = function(grunt) {
+    var out = fs.openSync('./out.log', 'a');
+    var err = fs.openSync('./out.log', 'a');
     var opts = {
         stdout: function (data) {
             grunt.log.writeln(data);
         },
         stderr: function (data) {
             grunt.log.error(data);
-        }
+        },
+        detached: true,
+        stdio: [ 'ignore', out, err ]
     };
 
     grunt.registerMultiTask('gorun', 'Run Go programs.', function() {
         var src = this.data.src;
-        var proc = spawn('go', ['run', src, '&'], opts);
-        var done = this.async();
+        var commandText = "go run "+src;
 
-        console.log('go run '+src+' &');
+        console.log("Executing "+commandText);
+
+        process.env.GOOS="darwin";
+        var proc = spawn('go', ['run', src], opts);
+        proc.unref();
+
+        var done = this.async();
 
         proc.on('stdout', function(data) {
             opts.stdout(data);
@@ -32,6 +42,8 @@ module.exports = function(grunt) {
         });
 
         proc.on('exit', function (status) {
+            grunt.log.error(proc.stdout._readableState.buffer);
+            grunt.log.error(proc.stderr._readableState.buffer);
             if (status !== 0) {
                 grunt.fail.fatal("Failure executing go run with exit code "+status+".");
             }
